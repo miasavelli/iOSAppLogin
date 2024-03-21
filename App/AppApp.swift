88a81b8aa0079e6a05b802aa2070301252f4616d
Mia Savelli <miasavelli@Mias-MBP.attlocal.net>
2024-03-20 20:52:28 -0500

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Mia Savelli on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
