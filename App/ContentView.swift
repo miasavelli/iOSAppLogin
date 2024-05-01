68e21cca9a00b6f661551fcefb3fe324ce4cfdf3
Mia Savelli <miasavelli@Mias-MBP.attlocal.net>
2024-05-01 18:16:27 -0500

    first commit

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -8,16 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationView {
+            ZStack {
+                Color.purple
+                    .ignoresSafeArea()
+
+                GeometryReader { gr in
+                    RoundedRectangle(cornerRadius: 10)
+                        .rotation(Angle(degrees: 60))
+                        .foregroundColor(.indigo.opacity(0.2))
+                    
+                    
+                    RoundedRectangle(cornerRadius: 10)
+                        .scale(x:30, y:80, anchor: UnitPoint(x:1,y:0))
+                        .rotation(Angle(degrees: 60))
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                
+                }
+                
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                        .foregroundColor(.white)
+                        
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width:300, height:50)
+                        .background(Color.white.opacity(0.6))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    TextField("Password", text: $password)
+                        .padding()
+                        .frame(width:300, height:50)
+                        .background(Color.white.opacity(0.6))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Spacer()
+                            
+                    
+                } // GeometryReader
+
+                
+            } // ZStack
+        } // NavigationView
+    } // body
+} // ContentView
 
 #Preview {
     ContentView()
